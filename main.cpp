bfef32a87863d827083b4ca04f60a04c155244b6
falarion08 <barrion.ryanonil@gmail.com>
2023-07-01 22:46:26 +0800

    Initial Computation For the Bin
    Problem : All arrays have the same value

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ int main() {
 
 	Size img_size = image_pad.size();
 
-	// cout << image_pad.rows << "\t" << image_pad.cols << endl;
+	cout << image_pad.rows << "\t" << image_pad.cols << endl;
 
 	// imshow("Image", image_pad);
 
@@ -35,53 +35,91 @@ int main() {
 
 	Sobel(image_pad, x_grad, CV_32FC1, 0, 1, 1);
 	Sobel(image_pad, y_grad, CV_32FC1, 1, 0, 1);
+	
+	Mat x_grad_abs = cv::abs(x_grad);
+	Mat y_grad_abs = cv::abs(y_grad);
 
-	// Gives 2 matrices 
 	Mat mag, dir;
-	cartToPolar(x_grad, y_grad, mag, dir, true);
+	cartToPolar(x_grad_abs, y_grad_abs, mag, dir, true);
 
+	cout << "\n\n" << dir.at<float>(12,120) << "\n\n";
 	// display images
 	// imshow("X", x_grad);
 	// imshow("Y", y_grad);
 	// imshow("Magnitude", mag);
 
 	// Determines how many histograms needed for each cell
-	const int nBin_rows = (image_pad.rows / 8) * (image_pad.cols / 8);
+	const int ncell_rows = image_pad.rows / 8;
+	const int ncell_cols = image_pad.cols / 8;
+	const int nBin_size = ncell_rows * ncell_cols;
 	
 	// Create 9x1 bins for each cell 
-	double ***HOGBin = new double**[nBin_rows];
-	for(int i = 0; i < nBin_rows; ++i){
-		HOGBin[i] = new double*[nBin_rows];
+	double ***HOGBin = new double**[nBin_size];
+	for(int i = 0; i < nBin_size; ++i){
+		HOGBin[i] = new double*[nBin_size];
 	}
 
-	for(int i = 0; i < nBin_rows; ++i){
-		for(int j = 0; j < nBin_rows; ++j){
+	for(int i = 0; i < ncell_rows; ++i){
+		for(int j = 0; j < ncell_cols; ++j){
 			HOGBin[i][j] = new double[9] {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
 		}
 	}
 
+	
 	/*
 		Initialize the Bins
 		Binning Method : Method 4 
 		Source: https://www.analyticsvidhya.com/blog/2019/09/feature-engineering-images-introduction-hog-feature-descriptor/#h-step-4-calculate-histogram-of-gradients-in-8x8-cells-9x1
+		
+		Resized image is N x N in size
 	*/
 	int bin_key = 0; 
-	for(int index = 0; index < nBin_rows; ++index){
-		// First two for loops traversing each cell 
-		for(int i = 0; i < image_pad.rows; i +=8){
-			for(int j = 0; j < image_pad.cols; j += 8){
+	double angle = 0.0, bin_value = 0.0;
+	const double bins[9] ={0.0,20.0,40.0,60.0,80.0,100.0,120.0,140.0,160.0};
+
+	for (int row_index = 0; row_index < ncell_rows; ++row_index){
+		for(int col_index = 0; col_index < ncell_cols; ++col_index){
+			
+			// Traverse each cell row-wise
+			for(int i = 0; i < dir.rows; i += 8){
+				for(int j = 0; j < dir.cols; j+=8){
 				
-				// Perform binning of each cell
-				for(int x = i; x < i + 8; ++x){
-					for (int y = j; y < j + 8; ++y){
+				// Compute for HOG Bin of each cell
+				for (int x = i; x < i + 8; ++x){
+					for(int y = j; y < j + 8; ++y){
+						
+						angle = dir.at<float>(x,y);
+						bin_key = angle/20;
 
-						// cout << "\n\n" << dir.at<float>(x,y) << "\n\n";
+						bin_value = ((angle - bins[bin_key])/ 20.0) * mag.at<float>(x,y);
 
+						switch(bin_key){
+							case 8:
+							// If vector direction is between 160 and 180 wrap around 0 
+							HOGBin[row_index][col_index][0] += bin_value;
+							break;
+							
+							default:
+							HOGBin[row_index][col_index][bin_key + 1] += bin_value;
+							break;
+						}
+
+						HOGBin[row_index][col_index][bin_key] += mag.at<float>(x,y) - bin_value;
 					}
+				}
+
 				}
 			}
 		}
+	}
 
+		for(int i = 0; i < ncell_rows; ++i){
+			for(int j = 0; j < ncell_cols; ++j){
+				for(int k = 0; k < 9; ++k){
+					cout << HOGBin[i][j][k] << " ";
+				}
+				cout << endl;
+		}
 	}
 	//todo: 2x2 blocking of histogram coefficients (into 1x36 coeffs)
 	
@@ -90,17 +128,19 @@ int main() {
 	//todo: normalization (L2 Norm) of resulting gradients
 
 
-	
+	// cout << dir ;
 	//todo: display HOG
 
 	// Free memory
-	for (int i = 0; i < nBin_rows; ++i){
-		for(int j = 0; j < nBin_rows; ++j){
+	for (int i = 0; i < ncell_rows; ++i){
+		for(int j = 0; j < ncell_cols; ++j){
 			delete[] HOGBin[i][j];
 		}
 		delete[] HOGBin[i];
 	}
 	delete[] HOGBin;
+	
+	cout << "eop" << "\n";
 
 	waitKey(0);
 	return 0;
