d86847850013330cdedef1733244b0742d9ddbad
falarion08 <barrion.ryanonil@gmail.com>
2023-08-02 14:02:13 +0800

    Created a single function for feature extraction

--- main.cpp
+++ main.cpp
@@ -227,7 +227,185 @@ void visualizeHOG(const cv::Mat& input_image,double * HOGFeatures, int ncell_row
 //     cv::waitKey(0);
 // }
 
+double *getHOGFeatures(Size img_size, Mat img_pad){
 
+	
+	/************************************************************
+	*					2. Computing Gradients
+	*************************************************************/
+
+	Mat x_grad = Mat(img_size, CV_32FC1);
+	Mat y_grad = Mat(img_size, CV_32FC1);
+
+	
+	//initialize first row as all 0's
+	for (int j = 0; j < img_pad.cols; j++)
+		x_grad.at<float>(0, j) = 0;
+
+	//compute x gradients
+	float top, bottom;
+	for (int i = 1; i < img_pad.rows - 1; i++) {
+		for (int j = 0; j < img_pad.cols; j++) {
+			bottom = img_pad.at<float>(i + 1, j);
+			top = img_pad.at<float>(i - 1, j);
+
+			x_grad.at<float>(i, j) = bottom - top;
+		}
+	}
+
+	//initialize first column as all 0's
+	for (int i = 0; i < img_pad.rows; i++)
+		y_grad.at<float>(i, 0) = 0;
+		
+
+	//compute y gradients
+	float left, right;
+	for (int i = 0; i < img_pad.rows; i++) {
+		for (int j = 1; j < img_pad.cols - 1; j++) {
+			right = img_pad.at<float>(i, j + 1);
+			left = img_pad.at<float>(i, j - 1);
+
+			y_grad.at<float>(i, j) = right - left;
+		}
+	}
+	
+	/************************************************************
+	*					3. Computing Polar values
+	*************************************************************/
+
+
+	Mat mag = Mat(img_size, CV_32FC1); 
+	Mat dir = Mat(img_size, CV_32FC1);
+
+	float x_val, y_val, res_dir;
+	double pi = atan(1) * 4;
+	
+	//compute magnitude and direction of cartesian coordinates x_val and y_val
+	for (int i = 0; i < x_grad.rows; i++) {
+		for (int j = 0; j < x_grad.cols; j++) {
+			x_val = x_grad.at<float>(i, j);
+			y_val = y_grad.at<float>(i, j);
+
+			mag.at<float>(i, j) = sqrt(pow(x_val, 2) + pow(y_val, 2));
+			res_dir = atan2(y_val, x_val) * 180/pi;
+			
+			//if resulting direction is < 0, negate by adding 180
+			if (res_dir < 0) res_dir += 180;
+			dir.at<float>(i, j) = res_dir;
+		}
+	}
+
+	/************************************************************
+	*			4. Binning Gradients to Angle bins
+	*************************************************************/
+
+	// Determines how many blocks are needed for the image
+	const int ncell_rows = img_pad.rows / 8;
+	const int ncell_cols = img_pad.cols / 8;
+	const int nBin_size = ncell_rows * ncell_cols;
+	
+	// initialize 9x1 bins corresponding to each block on the image
+	double ***HOGBin = new double**[ncell_rows];
+	for(int i = 0; i < ncell_rows; ++i){
+		HOGBin[i] = new double* [ncell_cols];
+		for (int j = 0; j < ncell_cols; ++j) {
+			HOGBin[i][j] = new double[9] {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+		}
+	}
+
+	/*
+	Initialize the Bins
+	Binning Method : Method 4
+	Reference: https://www.analyticsvidhya.com/blog/2019/09/feature-engineering-images-introduction-hog-feature-descriptor/#h-step-4-calculate-histogram-of-gradients-in-8x8-cells-9x1
+
+	* bins are are stored sequentially - index 0 = bin 0 and index 8 = bin 160
+	* Magnitude of the corresponding pixel is distributed
+	*/
+	int bin_key = 0;
+	int bin_key_hi = 0, bin_key_low = 0;
+	double mag_val = 0.0, angle = 0.0, bin_value_lo = 0.0, bin_value_hi = 0.0;
+	const double bins[10] = { 0.0, 20.0, 40.0, 60.0, 80.0, 100.0, 120.0, 140.0, 160.0, 180.0 };
+	
+	int HOG_row = 0, HOG_col = 0;
+
+	// Traverse each cell in the original data
+	for (int i = 0; i < dir.rows; i++) {
+		for (int j = 0; j < dir.cols; j++) {
+			//get corresponding HOG bin block
+			HOG_row = i / 8;
+			HOG_col = j / 8;
+
+			//angle 
+			angle = dir.at<float>(i, j);
+			//mag
+			mag_val = mag.at<float>(i, j);
+
+			// Round down to get which bin the direction belong to.
+			bin_key = angle / 20;
+			bin_key %= 9;
+
+			bin_key_hi = bins[bin_key+1];
+			bin_key_low = bins[bin_key];
+
+			//special case for 180 - move value to 0 bin (bins wrap around)
+			if (angle >= 180.0) {
+				angle = 0;
+			}
+
+			//equally divide contributions to different angle bins
+			bin_value_lo = ((bin_key_hi- angle) / 20.0) * mag_val;
+			bin_value_hi = ((bin_key_low - angle)/ 20.0) * mag_val;
+
+			//add value to bin
+			HOGBin[HOG_row][HOG_col][bin_key] += bin_value_lo;
+			HOGBin[HOG_row][HOG_col][(bin_key + 1) % 9] += bin_value_hi;
+		}
+	}
+
+	/************************************************************
+	*			4. Normalize Gradients in a 16x16 cell
+	*************************************************************/
+
+	// Determine number of features
+	
+	const int features = (ncell_rows - 1) * (ncell_cols - 1) * 36;
+	double *HOGFeatures = new double[features];
+
+		int feature_index = 0;
+		for (int i = 0; i < ncell_rows -1 ; i++) {
+			for (int j = 0; j < ncell_cols - 1; j++) {
+
+				for (int x = i; x < i + 2; ++x){
+					for(int y = j; y < j + 2; ++y){
+
+						for (int k = 0; k < 9; k++){
+							// Append the histogram of each cell
+							HOGFeatures[feature_index++] = HOGBin[x][y][k];
+						}
+				}
+			}
+			// Performs L2 normalization from index (feature_index - 36) to feature_index based on the current value in the loop
+				int start = feature_index - 36;
+				double k = 0;
+
+				// Get sum of squares
+				for (int i = start; i < feature_index; ++i){
+					k += pow(HOGFeatures[i], 2);
+				}
+
+				k = sqrt(k); 
+
+
+				// Divide each element from the resulting value derived from k
+				for (int i = start; i < feature_index; ++i){
+					HOGFeatures[i] = HOGFeatures[i] /k;
+				}
+		}
+	}
+	
+
+	return HOGFeatures;
+}
 
 int main() {
 
@@ -253,92 +431,19 @@ int main() {
 
 	Size img_size = image_pad.size();
 
-	cout << image_pad.rows << "\t" << image_pad.cols << "\n" << endl;
+	// cout << image_pad.rows << "\t" << image_pad.cols << "\n" << endl;
 
-	
-	/************************************************************
-	*					2. Computing Gradients
-	*************************************************************/
-
-	Mat x_grad = Mat(img_size, CV_32FC1);
-	Mat y_grad = Mat(img_size, CV_32FC1);
-
-	compute_gradients(x_grad, y_grad, image_pad);
-
-
-	/************************************************************
-	*					3. Computing Polar values
-	*************************************************************/
-
-	Mat mag = Mat(img_size, CV_32FC1); 
-	Mat dir = Mat(img_size, CV_32FC1);
-
-	compute_polar(mag, dir, x_grad, y_grad);
-
-	for (int i = 0; i < 8; i++) {
-		for (int j = 0; j < 8; j++)
-			cout << dir.at<float>(i, j) << "\t";
-
-		cout << endl;
-	}
-	cout << endl;
-
-	/************************************************************
-	*			4. Binning Gradients to Angle bins
-	*************************************************************/
-
-	// Determines how many blocks are needed for the image
 	const int ncell_rows = image_pad.rows / 8;
 	const int ncell_cols = image_pad.cols / 8;
-	const int nBin_size = ncell_rows * ncell_cols;
-	
-	// initialize 9x1 bins corresponding to each block on the image
-	double ***HOGBin = new double**[ncell_rows];
-	for(int i = 0; i < ncell_rows; ++i){
-		HOGBin[i] = new double* [ncell_cols];
-		for (int j = 0; j < ncell_cols; ++j) {
-			HOGBin[i][j] = new double[9] {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
-		}
-	}
-
-	bin_gradients(HOGBin, mag, dir);
-
-	// for (int i = 0; i < ncell_rows; i++) {
-	// 	for (int j = 0; j < ncell_cols; j++) {
-	// 		for (int k = 0; k < 9; k++)
-	// 			cout << HOGBin[i][j][k] << "\t";
-	// 		cout << endl;
-	// 	}
-	// 	cout << endl;
-	// }
-	// cout << endl;
-
-
-	/************************************************************
-	*			4. Normalize Gradients in a 16x16 cell
-	*************************************************************/
-
-	// Determine number of features
-	
+ 
+	double* HOGFeatures = getHOGFeatures(img_size,image_pad);
 	const int features = (ncell_rows - 1) * (ncell_cols - 1) * 36;
-	double *HOGFeatures = new double[features];
-
-	normalizeGradients(HOGFeatures, HOGBin, ncell_rows, ncell_cols);
-
-	//todo: display HOG
 
 	for(int i = 0; i < features; ++i){
 		cout << HOGFeatures[i] << "\n";
 	}
 	visualizeHOG(image_pad,HOGFeatures, ncell_rows, ncell_cols);
 	// Free memory
-	for (int i = 0; i < ncell_rows; ++i){
-		for(int j = 0; j < ncell_cols; ++j){
-			delete[] HOGBin[i][j];
-		}
-		delete[] HOGBin[i];
-	}
-	delete[] HOGBin;
 	
 	delete[] HOGFeatures; 
 	waitKey(0);
