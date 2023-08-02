fd30a987ab550a6c8c5a0c11f57b9d1261cb92d8
mayksel <michael_geronimo@dlsu.edu.ph>
2023-08-02 21:57:33 +0800

    fixed

--- hog_visualize.h
+++ hog_visualize.h
@@ -2,7 +2,11 @@
 #define HOG_VISUALIZE_H
 
 #include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 
-void visualizeHOG(const cv::Mat& input_image, double* HOGFeatures, double*** HOGBin, int ncell_rows, int ncell_cols, int num_bins);
+using namespace cv;
 
-#endif 
+void visualizeHOG(const Mat& input_image, double* HOGFeatures, double*** HOGBin, int ncell_rows, int ncell_cols, int num_bins);
+
+#endif // HOG_VISUALIZE_H
